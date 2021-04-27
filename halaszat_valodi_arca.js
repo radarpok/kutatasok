98cd2ce69f54a23733d8cb1ec6873080bc30585d
radarpok <vdiana.m8@gmail.com>
2021-04-27 15:18:30 +0200

    processing datas of the documentary 'Seaspiracy' by Netflix

---
+++ halaszat_valodi_arca.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-multi-str */
+module.exports.tengerbiologia=()=>{
+
+let halaszat_valodi_arca = [{
+
+    globalis_felmelegedes_fo_okai: {
+        1: "tengerben hagyott műanyag halászhálók",
+        2: "cápa- és delfinvadászat",
+        3: "'delfinbiztos' halászati címke egyszerűen lefizetés kérdése, ezesetben nem ellenőrzik"
+    },
+    reszletezett_okok: {
+        1: "a delfinekre és cápákra 'vetélytársként' tekintenek a halak kifogásával kapcsolatban, ennek és lefizetések okán \
+            mészárolhatják őket a halipar tovább-működtetése érdekében",
+        2: "a több focipályányi méretű halászhálók egyszerűen ledörzsölik a tenger fenekén élő korallzátonyokat és élővilágot a húzásuk közben",
+        3: "cápák irtása az uszonyukért: nem finom, iszonyat drága, csupán presztízskérdéseket dönt el",
+        4: "'mellékfogásként' megölt nagytestű állatok a tápláléklánc fokozatos felbomlását eredményezik",
+        5: "tápláléklánc felbomlása: ha a legfelső réteg fogyatkozik az alsó szaporodik, és az alatta levőt kezdi fogyasztani túlzott mértékben..stb",
+        6: "a tengeri élőlények a mozgása (felszín -> mély -> felszín -> mély...) olyan mértékű energiát (hőt) mozgat meg, mint a föld összes \
+            hurrikánja, cunamija, tüze együttvéve; ezért ha az élőlények fogyatkozásával ez az energia csökken, lassul, az a felszíni hő lassú \
+            de biztos növekedésével jár",
+        7: "a tengerek és óceánok kötik meg legnagyobb mértékben a CO2-t (ezerzer többet, mint az összes esőerdő együttvéve) a bioszférájukkal, \
+            ha ennnek hatékonyságából akár csak 1%-ot is veszít, az olyan, mint 17millió autónak a napi CO2-kibocsátásának az el-nem nyelése",
+        8: "a műnyag szívószálakra fogni a globális felmelegedés egyik fő okát (a legnagyobb bűnbakként kiáltják ki az aktivista szervezetek) \
+            olyan, mintha a fogpiszkálók gyártását okolnánk az erdőtüzek fő okaként"
+    },
+    megoldasok: {
+        1: "halfogyasztás csökkentése?",
+        2: "rájönni hogy lehet még pótolni az omega3 zsírsavat",
+        3: "rájönni mik a fő okai az omega3 zsírsav-hiánynak",
+        4: "megbizonyosodni afelől hogy a fentiek (alapja: Netflix - Seaspiracy ('A fenntartható halászat valódi arca') igazak, utánajárni",
+        5: "továbbadni amennyiben igaz, rájönni a túlzásokra ha vannak és azoknak okára"
+    }
+}]
+return console.log(halaszat_valodi_arca)
+};
